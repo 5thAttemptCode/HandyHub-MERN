035950264fc830bf62e29fdd6049826fd7098c79
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-17 22:21:02 +0100

    Add files via upload

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Register from './pages/register'
+import Navbar from './components/nav'
+import Mason from './pages/explore/mason'
+import Electrician from './pages/explore/electrician'
+import Painter from './pages/explore/painter'
+import Carpenter from './pages/explore/carpenter'
+import Roofer from './pages/explore/roofer'
+import Plumber from './pages/explore/plumber'
 
 
 export default function App() {
+  
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route path="/" element={ <Home />} />
-          <Route path="/register" element={ <Register />} />
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/mason" element={<Mason />} />
+        <Route path="/electrician" element={<Electrician />} />
+        <Route path="/painter" element={<Painter />} />
+        <Route path="/carpenter" element={<Carpenter />} />
+        <Route path="/roofer" element={<Roofer />} />
+        <Route path="/plumber" element={<Plumber />} />
+      </Routes>
+    </Router>
   )
 }
