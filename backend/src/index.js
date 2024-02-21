28db9c8d60a151b9d874275770ae6b4352aa7026
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-21 21:48:57 +0100

    Add files via upload

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,8 +1,9 @@
-import express from 'express'
 import cors from 'cors'
+import express from 'express'
+import dotenv from 'dotenv'  
 import mongoose from 'mongoose'
 import { userRouter } from './routes/authRoutes.js'
-import dotenv from 'dotenv'               
+             
 
 dotenv.config()  
 
@@ -14,10 +15,16 @@ app.use(cors())
 
 app.use("/auth", userRouter)
 
+mongoose.set('debug', true)
+
 //DB connection
-mongoose.connect(process.env.VITE_MONGO_URL)
+mongoose.connect(process.env.VITE_MONGO_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log("DATABASE CONNECTED"))
 .catch((err) => console.log("ERROR CONNECTION DB", err))
 
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!')
+})
 
 app.listen(3001, () => console.log("Server is runnin 100 miles"))
