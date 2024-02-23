eb375db3e5b8adf3c0e5c231d954f0452d016c8c
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-23 22:20:07 +0100

    Add files via upload

--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -1,51 +1,38 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import './style.css'
-import { Link } from 'react-router-dom'
-import DropdownMenu from '../dropdownMenu'
-import ExploreLink from './navComponents/exploreLink'
+import { Tabs } from '@mui/base/Tabs';
+import { TabsList } from '@mui/base/TabsList';
+import { TabPanel } from '@mui/base/TabPanel';
+import { Tab } from '@mui/base/Tab';
+import Register from './authComponents/register'
+import Login from './authComponents/login'
 
 
-export default function Navbar() {
-  const [menuVisible, setMenuVisible] = useState(false)
-  const dropdownRef = useRef(null)
-
-  const handleLinkClick = () => {
-    setMenuVisible(null)
-  }
-
-  const handleClickOutside = event => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setMenuVisible(false)
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => document.removeEventListener("mousedown", handleClickOutside)
-  }, [])
-
-
+export default function Auth() {
   return (
-    <nav>
-      <Link className='logo' to="/">Handy<span>Hub</span></Link>
-      <ul>
-        <DropdownMenu
-          buttonContent="Explore "
-          menuVisible={menuVisible}
-          setMenuVisible={setMenuVisible}
-          handleLinkClick={handleLinkClick}
-        >
-          <ExploreLink to="/electrician" clickHandler={handleLinkClick} title="Electrician" phrase="Exquisite Electrical Solutions" />
-          <ExploreLink to="/roofer" clickHandler={handleLinkClick} title="Roofer" phrase="Roofs done right, guaranteed tight!" />
-          <ExploreLink to="/plumber" clickHandler={handleLinkClick} title="Plumber" phrase="A flush beats a full house" />
-          <ExploreLink to="/carpenter" clickHandler={handleLinkClick} title="Carpenter" phrase="Elevating Spaces, Elevating Life" />
-          <ExploreLink to="/painter" clickHandler={handleLinkClick} title="Painter" phrase="Making Your Vision a Vibrant Reality" />
-          <ExploreLink to="/mason" clickHandler={handleLinkClick} title="Mason" phrase="Secrets hidden in plain sight" />
-        </DropdownMenu>
-        <Link className='hanhub' to="/become-a-handyhubber">Become a HandyHubber</Link>
-        <Link to="/auth">Sign Up</Link>
-        <Link to="/auth">Join</Link>
-      </ul>
-    </nav>
+    <div className='auth'>
+      <Tabs defaultValue={1}>
+        <TabPanel value={1}>
+          <Login />
+          <span>
+            <p>Haven't signed up yet? Sign up&nbsp;</p>
+            <TabsList>
+              <Tab value={2}>HERE</Tab>
+            </TabsList>
+          </span>
+        </TabPanel>
+        <TabPanel value={2}>
+          <Register />
+          <span>
+            <p>Already have an Account? Login&nbsp;</p>
+            <TabsList>
+              <Tab value={1}>HERE</Tab>
+            </TabsList>
+          </span>
+        </TabPanel>
+      </Tabs>
+    </div>
   )
 }
+
+  
