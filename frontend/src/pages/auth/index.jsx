55600d0b073fdfab887bce9038a94b865dabbdc9
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-02-26 22:16:38 +0100

    Add files via upload

--- frontend/src/pages/auth/index.jsx
+++ frontend/src/pages/auth/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './style.css'
 import { Tabs } from '@mui/base/Tabs';
 import { TabsList } from '@mui/base/TabsList';
@@ -6,31 +6,38 @@ import { TabPanel } from '@mui/base/TabPanel';
 import { Tab } from '@mui/base/Tab';
 import Register from './authComponents/register'
 import Login from './authComponents/login'
+import { AuthContext } from '../../context/AuthContext'
 
 
 export default function Auth() {
+
+  const { activeTab, setActiveTab } = useContext(AuthContext)
+  
   return (
     <div className='auth'>
-      <Tabs defaultValue={1}>
-        <TabPanel value={1}>
-          <Login />
-          <span>
-            <p>Haven't signed up yet? Sign up&nbsp;</p>
-            <TabsList>
-              <Tab value={2}>HERE</Tab>
-            </TabsList>
-          </span>
-        </TabPanel>
-        <TabPanel value={2}>
-          <Register />
-          <span>
-            <p>Already have an Account? Login&nbsp;</p>
-            <TabsList>
-              <Tab value={1}>HERE</Tab>
-            </TabsList>
-          </span>
-        </TabPanel>
-      </Tabs>
+      <div className='auth-container'>
+        <img src="https://images.pexels.com/photos/5691637/pexels-photo-5691637.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+        <Tabs className='tabs' value={activeTab} onChange={(event, newValue) => setActiveTab(newValue)}>
+          <TabPanel value={1}>
+            <Login />
+            <span>
+              <p>Haven't signed up yet? Sign up&nbsp;</p>
+              <TabsList>
+                <Tab value={2}>HERE</Tab>
+              </TabsList>
+            </span>
+          </TabPanel>
+          <TabPanel value={2}>
+            <Register />
+            <span>
+              <p>Already have an Account? Login&nbsp;</p>
+              <TabsList>
+                <Tab value={1}>HERE</Tab>
+              </TabsList>
+            </span>
+          </TabPanel>
+        </Tabs>
+      </div>
     </div>
   )
 }
