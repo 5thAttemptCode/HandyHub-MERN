f654ec3d5ed9536632632b181a2e4622d021faaf
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-07 11:28:41 +0100

    updated AuthProvider

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,13 +1,20 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useContext, useState } from 'react'
 
 export const AuthContext = createContext()
 
+export function useAuth() {
+  return useContext(AuthContext)
+}
+
 export const AuthProvider = ({ children }) => {
     
-  const [activeTab, setActiveTab] = useState(1)
+  const [ activeTab, setActiveTab ] = useState(1)
+  const [ isAuthenticated, setIsAuthenticated ] = useState(false)
+  const [ user, setUser ] = useState(null)
+  const [ handyHubberUser, setHandyHubberUser ] = useState(null)
 
   return (
-    <AuthContext.Provider value={{ activeTab, setActiveTab }}>
+    <AuthContext.Provider value={{ activeTab, setActiveTab, isAuthenticated, setIsAuthenticated, user, setUser, handyHubberUser, setHandyHubberUser }}>
       {children}
     </AuthContext.Provider>
   )
