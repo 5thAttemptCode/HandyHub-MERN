876e91fe192a4a9005ee6ec2adb97bee93e7a922
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-07 11:27:52 +0100

    Updated dom-routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,61 +1,52 @@
 import React, { useContext } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
-import { CartProvider } from './context/cartContext'
-import { FloorMaterialProvider } from './context/floorButtonContext'
-import { OrderProvider } from './context/orderContext'
-import { Toaster } from 'react-hot-toast'
-import { UserContext, UserContextProvider } from './context/userContext'
-import axios from 'axios'
-import Checkout from './pages/checkout'
-import Configurator from './pages/configurator'
-import FloorMaterials from './pages/floorMaterials'
-import Footer from './components/footer'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Home from './pages/home'
-import Login from './pages/user/Login'
-import Nav from './components/nav'
-import Register from './pages/user/Register'
-import ScrollToTop  from './utils/scrollToTop'
+import Navbar from './components/nav'
+import Mason from './pages/explore/mason'
+import Electrician from './pages/explore/electrician'
+import Painter from './pages/explore/painter'
+import Carpenter from './pages/explore/carpenter'
+import Roofer from './pages/explore/roofer'
+import Plumber from './pages/explore/plumber'
+import HandyHubber from './pages/handyHubberAuth'
+import UserAuth from './pages/userAuth'
+import { AuthContext, AuthProvider } from './context/AuthContext'
 import UserProfile from './pages/userProfile'
-
-
-axios.defaults.baseURL = import.meta.env.VITE_BASE_URL
-axios.defaults.withCredentials = true
+import HandyHubberProfile from './pages/handyHubberProfile'
 
 
 export default function App() {
+
   return (
-    <UserContextProvider>
-      <FloorMaterialProvider>
-        <CartProvider>
-          <OrderProvider>
-            <BrowserRouter>
-              <Nav />
-              <ScrollToTop />
-              <Toaster position="top-right" toastOption={{duration: 1000}} />
-              <Routes>
-                <Route path="/" element={<Home />} />          
-                <Route path="/register" element={<Register />} />
-                <Route path="/configurator" element={<Configurator />} />
-                <Route path="/checkout" element={<Checkout />} />
-                <Route path="/floor-materials" element={<FloorMaterials />} />
-                <Route path="/profile" element={<ProfileRoute />} />
-                <Route path="/login" element={<LoginRoute />} />    
-              </Routes>
-              <Footer />
-            </BrowserRouter>
-          </OrderProvider>
-        </CartProvider>
-      </FloorMaterialProvider>
-    </UserContextProvider>
+    <AuthProvider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<UserAuth />} />
+          <Route path="/become-a-handyhubber" element={<HandyHubber />} />
+
+          <Route path="/user-profile" element={<ProfileRoute />} />
+          <Route path="/handy-hubber-profile" element={<HandyHubberRoute />} />
+
+          <Route path="/mason" element={<Mason />} />
+          <Route path="/electrician" element={<Electrician />} />
+          <Route path="/painter" element={<Painter />} />
+          <Route path="/carpenter" element={<Carpenter />} />
+          <Route path="/roofer" element={<Roofer />} />
+          <Route path="/plumber" element={<Plumber />} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   )
 }
 
 const ProfileRoute = () => {
-  const { user } = useContext(UserContext)
-  return user ? <UserProfile /> : <Navigate to="/login" />
+  const { user } = useContext(AuthContext)
+  return user ? <UserProfile /> : <Navigate to="/auth" />
 }
 
-const LoginRoute = () => {
-  const { user } = useContext(UserContext)
-  return user ? <Navigate to="/profile" /> : <Login />
+const HandyHubberRoute = () => {
+  const { handyHubberUser } = useContext(AuthContext)
+  return handyHubberUser ? <HandyHubberProfile /> : <Navigate to="/become-a-handyhubber" />
 }
