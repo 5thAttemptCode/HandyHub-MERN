a149eb2887b40bb891d34aec9ee1e36be8706c3e
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-07 16:35:34 +0100

    updated AuthContext

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -2,6 +2,8 @@ import React, { createContext, useContext, useState } from 'react'
 
 export const AuthContext = createContext()
 
+const initialHandyHubberUser = JSON.parse(localStorage.getItem('handyHubberUser')) || null
+
 export function useAuth() {
   return useContext(AuthContext)
 }
@@ -11,10 +13,19 @@ export const AuthProvider = ({ children }) => {
   const [ activeTab, setActiveTab ] = useState(1)
   const [ isAuthenticated, setIsAuthenticated ] = useState(false)
   const [ user, setUser ] = useState(null)
-  const [ handyHubberUser, setHandyHubberUser ] = useState(null)
+  const [ handyHubberUser, setHandyHubberUser ] = useState(initialHandyHubberUser)
 
   return (
-    <AuthContext.Provider value={{ activeTab, setActiveTab, isAuthenticated, setIsAuthenticated, user, setUser, handyHubberUser, setHandyHubberUser }}>
+    <AuthContext.Provider value={{ 
+      activeTab, 
+      setActiveTab, 
+      isAuthenticated, 
+      setIsAuthenticated, 
+      user, 
+      setUser, 
+      handyHubberUser, 
+      setHandyHubberUser 
+    }}>
       {children}
     </AuthContext.Provider>
   )
