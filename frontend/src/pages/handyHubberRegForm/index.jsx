7f6a27777a92859ef457c70e3ddbc3ce79551759
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-07 16:43:21 +0100

    added HandyHubberRegForm for register path

---
+++ frontend/src/pages/handyHubberRegForm/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function HandyHubberRegister() {
+  return (
+    <section className="handy-hubber-register">
+        <h1>HELLO</h1>
+    </section>
+  )
+}
