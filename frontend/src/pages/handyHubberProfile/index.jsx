c1197a1213074dbfaa86d7aa3ad0091762710373
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-07 11:29:37 +0100

    Added UserProfile and HandyHubberprofile

---
+++ frontend/src/pages/handyHubberProfile/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+
+export default function HandyHubberProfile() {
+  return (
+    <section className="handy-hubber-profile">
+      
+    </section>
+  )
+}
---
+++ frontend/src/pages/userProfile/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './style.css'
+
+
+export default function UserProfile() {
+  return (
+    <section className='user-profile'>
+
+    </section>
+  )
+}
