d09b75f580b62a01b3728eb730a4f762efb9d169
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-07 16:41:30 +0100

    Added logout function

--- frontend/src/pages/userProfile/index.jsx
+++ frontend/src/pages/userProfile/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './style.css'
+import { AuthContext } from '../../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function UserProfile() {
+
+  const { setUser } = useContext(AuthContext)
+
+  const navigate = useNavigate()
+
+  const logOut = () => {
+    setUser(null)
+    localStorage.removeItem('user')
+    navigate("/")
+  }
+
   return (
     <section className='user-profile'>
-
+      <button onClick={logOut}>logout</button>
     </section>
   )
 }
