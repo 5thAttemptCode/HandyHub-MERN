c0e6f9f808b7f8c760e355dc8246ab84261eb5f3
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-07 16:40:46 +0100

    Added logout function

--- frontend/src/pages/handyHubberProfile/index.jsx
+++ frontend/src/pages/handyHubberProfile/index.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import './style.css'
+import { AuthContext } from '../../context/AuthContext'
+import { useNavigate } from 'react-router-dom'
 
 
 export default function HandyHubberProfile() {
+
+  const { setHandyHubberUser } = useContext(AuthContext)
+
+  const navigate = useNavigate()
+
+  const logOut = () => {
+    setHandyHubberUser(null)
+    localStorage.removeItem('handyHubberUser')
+    navigate("/")
+  }
+
   return (
     <section className="handy-hubber-profile">
-      
+      <button onClick={logOut}>LOGOUT</button>
     </section>
   )
 }
