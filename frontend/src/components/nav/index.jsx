b2a957453b9abfe8f700e3d375cd38a3c4d57ee0
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-08 14:27:10 +0100

    cleaned up nav

--- frontend/src/components/nav/index.jsx
+++ frontend/src/components/nav/index.jsx
@@ -8,32 +8,8 @@ import { AuthContext } from '../../context/AuthContext'
 
 export default function Navbar() {
 
-  //toggle menu
-  const [menuVisible, setMenuVisible] = useState(false)
-
-  const handleLinkClick = () => {
-    setMenuVisible(null)
-  }
-
-
-  //Close menu on outside-click
-  const dropdownRef = useRef(null)
-
-  const handleClickOutside = event => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setMenuVisible(false)
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => document.removeEventListener("mousedown", handleClickOutside)
-  }, [])
-
-
   //AuthContext for login/register
   const { activeTab, setActiveTab } = useContext(AuthContext)
-
 
   return (
     <nav>
@@ -41,20 +17,17 @@ export default function Navbar() {
       <ul>
         <DropdownMenu
           buttonContent="Explore "
-          menuVisible={menuVisible}
-          setMenuVisible={setMenuVisible}
-          handleLinkClick={handleLinkClick}
         >
-          <ExploreLink to="/electrician" clickHandler={handleLinkClick} title="Electrician" phrase="Exquisite Electrical Solutions" />
-          <ExploreLink to="/roofer" clickHandler={handleLinkClick} title="Roofer" phrase="Roofs done right, guaranteed tight!" />
-          <ExploreLink to="/plumber" clickHandler={handleLinkClick} title="Plumber" phrase="A flush beats a full house" />
-          <ExploreLink to="/carpenter" clickHandler={handleLinkClick} title="Carpenter" phrase="Elevating Spaces, Elevating Life" />
-          <ExploreLink to="/painter" clickHandler={handleLinkClick} title="Painter" phrase="Making Your Vision a Vibrant Reality" />
-          <ExploreLink to="/mason" clickHandler={handleLinkClick} title="Mason" phrase="Secrets hidden in plain sight" />
+          <ExploreLink to="/electrician" title="Electrician" phrase="Exquisite Electrical Solutions" />
+          <ExploreLink to="/roofer" title="Roofer" phrase="Roofs done right, guaranteed tight!" />
+          <ExploreLink to="/plumber" title="Plumber" phrase="A flush beats a full house" />
+          <ExploreLink to="/carpenter" title="Carpenter" phrase="Elevating Spaces, Elevating Life" />
+          <ExploreLink to="/painter" title="Painter" phrase="Making Your Vision a Vibrant Reality" />
+          <ExploreLink to="/mason" title="Mason" phrase="Secrets hidden in plain sight" />
         </DropdownMenu>
         <Link className='hanhub' to="/become-a-handyhubber">Become a HandyHubber</Link>
         <Link to="/auth" onClick={() => setActiveTab(1)}>Login</Link>
-        <Link to="/auth" onClick={() => setActiveTab(2)}>Register</Link>
+        <Link to="/auth" onClick={() => setActiveTab(2)}>Sign Up</Link>
       </ul>
     </nav>
   )
