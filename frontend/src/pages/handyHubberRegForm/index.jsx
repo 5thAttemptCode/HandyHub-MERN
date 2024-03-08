2db13ff8b97c5b51214f924dc66321018b0c16ab
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-08 14:25:26 +0100

    imported dropdown-menu + styles

--- frontend/src/pages/handyHubberRegForm/index.jsx
+++ frontend/src/pages/handyHubberRegForm/index.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import './style.css'
+import DropdownMenu from '../../components/dropdownMenu'
+
 
 export default function HandyHubberRegister() {
-  return (
+
+  const [ selectedIndustry, setSelectedIndustry ] = useState("Industry")
+
+  const handleChange = (industry) => {
+    setSelectedIndustry(industry)
+  }
+
+  return ( 
     <section className="handy-hubber-register">
-        <h1>HELLO</h1>
+        <h1>Please fill out the form below</h1>
+        <label>Company Name</label>
+        <input type='text' />
+        <DropdownMenu buttonContent={selectedIndustry}>
+          <p onClick={() => handleChange("Roofer")}>Roofer</p>
+          <p onClick={() => handleChange("Mason")}>Mason</p>
+          <p onClick={() => handleChange("Plumber")}>Plumber</p>
+          <p onClick={() => handleChange("Electrician")}>Electrician</p>
+          <p onClick={() => handleChange("Carpenter")}>Carpenter</p>
+          <p onClick={() => handleChange("Painter")}>Painter</p>
+        </DropdownMenu>
     </section>
   )
 }
