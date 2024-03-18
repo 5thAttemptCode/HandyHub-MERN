47ff6c77e554e45c5cf9bc58a26de26c9f8f41d9
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-18 21:59:40 +0100

    .slice search in array

--- frontend/src/pages/home/components/iconLinks/index.jsx
+++ frontend/src/pages/home/components/iconLinks/index.jsx
@@ -1,24 +1,24 @@
 import React from 'react'
 import './style.css'
+import SubHeader from '@/components/subHeader'
+import { IndustryData } from '@/components/data' 
 import { Link } from 'react-router-dom'
-import { House, HouseSimple, Lightning, PaintBrushHousehold, Toilet, Wall } from '@phosphor-icons/react'
-import SubHeader from '../../../../components/subHeader'
 
 
 export default function IconLinks() {
-
-  const size = 50
 
   return (
     <div className='icon-links'>
       <SubHeader text="You need it, we've got it" />
       <div className="icon-links-container">
-        <IconLink to="/roofer" icon={<HouseSimple size={size} />} page="Roofer" />
-        <IconLink to="/mason" icon={<Wall size={size} />} page="Mason" />
-        <IconLink to="/electrician" icon={<Lightning size={size} />} page="Electrician" />
-        <IconLink to="/painter" icon={<PaintBrushHousehold size={size} />} page="Painter" />
-        <IconLink to="/carpenter" icon={<House size={size} />} page="Carpenter" />
-        <IconLink to="/plumber" icon={<Toilet size={size} />} page="Plumber" />
+      {IndustryData.map(item => 
+        <IconLink 
+          key={item.industryName}
+          to={`/${item.url}`} 
+          icon={item.icon}
+          page={item.industryName}
+        /> 
+      )}
       </div>
     </div>
   )
