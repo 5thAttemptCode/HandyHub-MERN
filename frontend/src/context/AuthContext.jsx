bf34e43548b55555ea8b515edb1853229c72e11f
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-03-20 21:10:42 +0100

    Change from Context to Zustand

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,32 +1,19 @@
-import React, { createContext, useContext, useState } from 'react'
+import { create } from 'zustand'
 
-export const AuthContext = createContext()
 
 const initialHandyHubberUser = JSON.parse(localStorage.getItem('handyHubberUser')) || null
 
-export function useAuth() {
-  return useContext(AuthContext)
-}
+// Define store
+export const useAuthStore = create((set) => ({
+  activeTab: 1,
+  setActiveTab: (tab) => set({ activeTab: tab }),
 
-export const AuthProvider = ({ children }) => {
-    
-  const [ activeTab, setActiveTab ] = useState(1)
-  const [ isAuthenticated, setIsAuthenticated ] = useState(false)
-  const [ user, setUser ] = useState(null)
-  const [ handyHubberUser, setHandyHubberUser ] = useState(initialHandyHubberUser)
+  isAuthenticated: false,
+  setIsAuthenticated: (auth) => set({ isAuthenticated: auth }),
 
-  return (
-    <AuthContext.Provider value={{ 
-      activeTab, 
-      setActiveTab, 
-      isAuthenticated, 
-      setIsAuthenticated, 
-      user, 
-      setUser, 
-      handyHubberUser, 
-      setHandyHubberUser 
-    }}>
-      {children}
-    </AuthContext.Provider>
-  )
-}
+  user: null,
+  setUser: (user) => set({ user }),
+
+  handyHubberUser: initialHandyHubberUser,
+  setHandyHubberUser: (user) => set({ handyHubberUser: user }),
+}))
