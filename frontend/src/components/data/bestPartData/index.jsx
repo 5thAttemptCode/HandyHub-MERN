cc0cb532ab0b67b8f635db990586eb2f26a1e747
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-25 21:17:33 +0200

    content-data for BestPart-component

---
+++ frontend/src/components/data/bestPartData/index.jsx
@@ -0,0 +1,18 @@
+export const bestPartData= [
+    {
+        title: "Stick to your budget",
+        bestText: "Find the right service for every price point. No hourly rates, just project-based pricing."
+    },
+    {
+        title: "Get quality work done quickly",
+        bestText: "Hand your project over to a talented freelancer in minutes, get long-lasting results."
+    },
+    {
+        title: "Pay when you're happy",
+        bestText: "Upfront quotes mean no surprises. Payments only get released when you approve."
+    },
+    {
+        title: "Count on 24/7 support",
+        bestText: "Our round-the-clock support team is available to help anytime, anywhere."
+    },
+]
