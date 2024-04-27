de32f67ac8cc7e7363231fd3b6a20e3690720efe
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-27 19:45:34 +0200

    imported LinkCarousel

--- frontend/src/pages/home/index.jsx
+++ frontend/src/pages/home/index.jsx
@@ -4,24 +4,27 @@ import IconLinks from './components/iconLinks'
 import { IndustryData } from '@/components/data'
 import { Link } from 'react-router-dom'
 import BestPart from './components/bestPart'
+import EmblaCarousel from '../../components/carousel'
+import LinkCarousel from './components/linkCarousel'
 
 
 export default function Home() {
   return (
     <>
-    <section className='home'>
-      <div className='header-container'>
-        <h1>Find the right <span>experts</span> for your home-build</h1>
-        <div className="quick-links">
-          Popular:
-          {IndustryData.slice(0, 4).map(item => (
-            <Link key={item.industryName} to={`/${item.url}`}>{item.industryName}</Link>
-          ))}
+      <section className='home'>
+        <div className='header-container'>
+          <h1>Find the right <span>experts</span> for your home-build</h1>
+          <div className="quick-links">
+            Popular:
+            {IndustryData.slice(0, 4).map(item => (
+              <Link key={item.industryName} to={`/${item.url}`}>{item.industryName}</Link>
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
-    <IconLinks />
-    <BestPart />
+      </section>
+      <IconLinks />
+      <BestPart />
+      <LinkCarousel />
     </>
   )
 }
