2af4060ffb626b4d19dc84763b7e34261d044324
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-28 13:21:36 +0200

    Added tags for searchbar

--- frontend/src/components/data/index.jsx
+++ frontend/src/components/data/index.jsx
@@ -10,46 +10,57 @@ export const IndustryData = [
     icon: <HouseSimple size={size} />,
     subText: "Roofs done right, guaranteed tight!",
     mainText: "Building a roof is such an exciting phase but it is also extremely crucial. Your actions and decisions may have long-term repercussions if not aligned to industry standards. Whether it is a new roof or a replacement that involves different types of roof construction, there are factors that you have to bear in mind.",
-    image: "https://images.pexels.com/photos/2980954/pexels-photo-2980954.jpeg"
+    image: "https://images.pexels.com/photos/2980954/pexels-photo-2980954.jpeg",
+    tags: ["tiles", "roof", "roofer", "wall", "bricks", "slate", "insolation", "leaky construction", "wood", "water"]
   },
+
   {
     industryName: "Electrician",
     url: "electrician",
     icon: <Lightning size={size} />,
     subText: "Exquisite Electrical Solutions",
     mainText: "An electrician is a tradesperson specializing in electrical wiring of buildings, transmission lines, stationary machines, and related equipment. Electricians may be employed in the installation of new electrical components or the maintenance and repair of existing electrical infrastructure.",
-    image: "https://images.pexels.com/photos/5691642/pexels-photo-5691642.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    image: "https://images.pexels.com/photos/5691642/pexels-photo-5691642.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["electricity", "light", "electronic", "cable", "wires", "light installation", "electric", "bulb", "cable renovation", "power"]
   },
+
   {
     industryName: "Plumber",
     url: "plumber",
     icon: <Toilet size={size} />,
     subText: "A flush beats a full house",
     mainText: "What is a plumber? A plumber is a construction professional who works on pipes and systems that carry water and waste throughout buildings. Common duties of a plumber include: Installing pipes into new building and ensuring they work properly.",
-    image: "https://images.pexels.com/photos/7244719/pexels-photo-7244719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    image: "https://images.pexels.com/photos/7244719/pexels-photo-7244719.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["water", "pipes", "leaky pipe", "pipe", "toilet", "leaky construction", "wall", "sanitary", "bathroom", "drain"]
   },
+
   {
     industryName: "Carpenter",
     url: "carpenter",
     icon: <House size={size} />,
     subText: "Elevating Spaces, Elevating Life",
     mainText: "Carpentry is one of the oldest construction trades and is in high demand. Carpenters use natural materials (wood/timber) to install wooden fixtures and fittings. As a carpenter you could be installing doors, floors and furniture in new builds, renovating or refitting existing structures, building sets for film and theatre companies and much more.",
-    image: "https://images.pexels.com/photos/5768031/pexels-photo-5768031.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    image: "https://images.pexels.com/photos/5768031/pexels-photo-5768031.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["wall", "insolation", "water", "color", "paint", "wood construction", "dry wall", "renovation", "wallpaper", "concrete"]
   },
+
   {
     industryName: "Painter",
     url: "painter",
     icon: <PaintBrushHousehold size={size} />,
     subText: "Making Your Vision a Vibrant Reality",
     mainText: "Painters apply paint, stain, and coatings to walls and ceilings, buildings, large machinery and equipment, and bridges and other structures.",
-    image: "https://images.pexels.com/photos/5799086/pexels-photo-5799086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    image: "https://images.pexels.com/photos/5799086/pexels-photo-5799086.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["wall", "paint thinner", "water", "color", "paint", "wallpaper", "concrete", "renovation", "brush", "paintjob"]
   },
+
   {
     industryName: "Mason",
     url: "mason",
     icon: <Wall size={size} />,
     subText: "Secrets hidden in plain sight",
     mainText: "Masonry consists of building structures from single units that are laid and bound together with mortar. Brick, stone and concrete blocks are the most common materials used in masonry construction. Masonry is a popular construction technique around the world, due to its many advantages.",
-    image: "https://images.pexels.com/photos/5512656/pexels-photo-5512656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    image: "https://images.pexels.com/photos/5512656/pexels-photo-5512656.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    tags: ["wall", "bricks", "slate", "wood construction", "concrete", "floor", "seiling", "renovation", "outdoor wall", "stone"]
   },
 ]
