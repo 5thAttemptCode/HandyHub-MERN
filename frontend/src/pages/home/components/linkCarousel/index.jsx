f208622e76a84fa7315d27f9a790d9d947ab26db
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-28 13:24:59 +0200

    updated props

--- frontend/src/pages/home/components/linkCarousel/index.jsx
+++ frontend/src/pages/home/components/linkCarousel/index.jsx
@@ -2,17 +2,21 @@ import React from 'react'
 import GridContainer from '../../../../components/gridContainer'
 import EmblaCarousel from '../../../../components/carousel'
 import { IndustryData } from '@/components/data'
+import SubHeader from '../../../../components/subHeader'
 
 
 export default function LinkCarousel() {
   return (
-    <GridContainer backgroundColor="rgb(14, 60, 66)">
+    <GridContainer backgroundColor="var(--gridContainerTwo)">
+      <SubHeader marginBottom="2rem" text="Popular services" />
       <EmblaCarousel
         Slider={IndustryData} 
         ElementOne={IndustryData.map(item => item.industryName)}
         ElementTwo={IndustryData.map(item => item.image)}
         ElementThree={IndustryData.map(item => item.url)}
+        ElementFour={IndustryData.map(item => item.subText)}
         LinkTo={true} 
+        SubText={true}
       />
     </GridContainer>
   )
