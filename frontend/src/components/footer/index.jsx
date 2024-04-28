b5dafed4ba97ff3cc8e0466c5c7279bc5fc316a6
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-28 13:30:03 +0200

    edited content and styles

--- frontend/src/components/footer/index.jsx
+++ frontend/src/components/footer/index.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import './style.css'
+import Logo from '../logo'
+import { InstagramLogo, LinkedinLogo, MetaLogo, PinterestLogo, TiktokLogo, TwitterLogo } from '@phosphor-icons/react'
+
 
 export default function Footer() {
+
+  const logoSize = 25
+
   return (
-    <div>
-      
-    </div>
+    <footer>
+      <div className="footer-container"></div>
+      <div className="footer-container">
+        <div className="footer-box">
+          <Logo />
+          <p>&copy; HandyHub International Ltd. 2024</p>
+        </div>
+        <div className="footer-box">
+          <a href="#"><TiktokLogo size={logoSize} /></a>
+          <a href="#"><InstagramLogo size={logoSize} /></a>
+          <a href="#"><LinkedinLogo size={logoSize} /></a>
+          <a href="#"><MetaLogo size={logoSize} /></a>
+          <a href="#"><PinterestLogo size={logoSize} /></a>
+          <a href="#"><TwitterLogo size={logoSize} /></a>
+        </div>
+      </div>
+    </footer>
   )
 }
