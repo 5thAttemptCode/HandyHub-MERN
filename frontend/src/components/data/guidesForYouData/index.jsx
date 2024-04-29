1dbb15f5382fbc5a9400c66e7b87ceee4a06368d
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-04-29 20:31:07 +0200

    .

---
+++ frontend/src/components/data/guidesForYouData/index.jsx
@@ -0,0 +1,24 @@
+const GuideForYouData = [
+    {
+        id: 1,
+        url: "guide-foryour-home-project-linkOne",
+        headline: "Reasons you need to hire experts for a renovation project",
+        image: "https://images.pexels.com/photos/3761508/pexels-photo-3761508.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+
+    {
+        id: 2,
+        url: "guide-foryour-home-project-linkTwo",
+        headline: "I See a Red Door and I Want to Paint it Blue ",
+        image: "https://images.pexels.com/photos/5031243/pexels-photo-5031243.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+
+    {
+        id: 3,
+        url: "guide-foryour-home-project-linkThree",
+        headline: "How to Hang Something Heavy",
+        image: "https://images.pexels.com/photos/2065650/pexels-photo-2065650.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+    },
+]
+
+export default GuideForYouData
