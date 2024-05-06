a37b6d4aaf0de51127e9ae04bb84d11f76bd6b3b
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-06 15:36:27 +0200

    replaced <Link /> with ModalButton

--- frontend/src/pages/home/components/joinHandyHub/index.jsx
+++ frontend/src/pages/home/components/joinHandyHub/index.jsx
@@ -1,15 +1,15 @@
 import React from 'react'
 import './style.css'
-import GridContainer from '../../../../components/gridContainer'
-import { Link } from 'react-router-dom'
+import GridContainer from '@/components/gridContainer'
+import ModalButton from '../../../handyHubberAuth/components/modal/components/modalButton'
 
 
 export default function JoinHandyHub() {
   return (
     <GridContainer>
       <div className="help">
-        <h1>Take your business to the <br /> <span>next level</span></h1>
-        <Link to="/">Join HandyHub</Link>
+        <h1>Take your business to the <span>next level</span></h1>
+        <ModalButton />
         <img src="/public/image.png" alt="" />
       </div>
     </GridContainer>
