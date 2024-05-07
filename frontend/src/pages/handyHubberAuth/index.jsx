a174110c87eafed4d52e60362c6206c5c0401a8c
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-07 19:48:42 +0200

    import and created new component

--- frontend/src/pages/handyHubberAuth/index.jsx
+++ frontend/src/pages/handyHubberAuth/index.jsx
@@ -3,15 +3,25 @@ import './style.css'
 import SubHeader from '@/components/subHeader'
 import SubHeaderSmall from '@/components/subHeaderSmall'
 import ModalButton from './components/modal/components/modalButton'
+import HowItWorks from './components/howItWorks'
+import AsideContainer from '../../components/asideContainer'
 
 
 export default function HandyHubber() {
 
   return (
-    <section className='handy-auth'>
-      <SubHeader text="Work Your Way" />
-      <SubHeaderSmall text="You bring the skill. We'll make earning easy." />
-      <ModalButton />
-    </section>
+    <>
+      <section className='handy-auth'>
+        <SubHeader text="Work Your Way" />
+        <SubHeaderSmall text="You bring the skill. We'll make earning easy." />
+        <ModalButton />
+      </section>
+      <HowItWorks />
+      <AsideContainer>
+        <SubHeaderSmall text="Sign up and create your first Gig today" />
+        <ModalButton />
+        <AsideContainer></AsideContainer>
+      </AsideContainer>
+    </>
   )
 }
