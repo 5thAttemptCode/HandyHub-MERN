f4f2690030842a861c27f0ffe20d11bac7d81201
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-08 20:48:51 +0200

    .

---
+++ frontend/src/components/data/faq/index.jsx
@@ -0,0 +1,24 @@
+const faqData = [
+    {
+        id: 1,
+        question: "What can I sell?",
+        answer: "You can offer any craftman ships service you wish as long as it's legal and complies with our terms. There are over five categories you can browse to get ideas."
+    },
+    {
+        id: 2,
+        question: "How does it work?",
+        answer: "You and a buyer agree on a project and a price. You both sign the contract, the buyer pays the money to HandyHub, and when the project is finished; you get your money! Its safe for both sides."
+    },
+    {
+        id: 3,
+        question: "How much does it cost?",
+        answer: "It's free to join HandyHub. There is no subscription required or fees to list your services. You keep 85% of each transaction."
+    },
+    {
+        id: 4,
+        question: "How do I getting paid",
+        answer: "Once you complete a buyer's project, the money is transferred to your account. No need to chase clients for payments and wait 60 or 90 days for a check."
+    },
+]
+
+export default faqData
