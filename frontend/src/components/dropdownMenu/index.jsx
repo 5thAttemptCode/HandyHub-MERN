daeb032d4b15af4deb719502df77c66e8bd4bee3
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-08 21:05:34 +0200

    replaced clickOutside function with custom-hook

--- frontend/src/components/dropdownMenu/index.jsx
+++ frontend/src/components/dropdownMenu/index.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 import './style.css'
 import { CaretDown } from '@phosphor-icons/react'
+import useClickOutside from '../../utliz/useClickOutside'
 
 
 export default function DropdownMenu(props) {
@@ -8,20 +9,15 @@ export default function DropdownMenu(props) {
   const { buttonContent, children, menuVisible: propMenuVisible = false } = props
   
   const dropdownRef = useRef(null)
+  const menuRef = useRef(null)
   const [ menuVisible, setMenuVisible ] = useState(propMenuVisible)
 
-  const handleClickOutside = event => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setMenuVisible(false)
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  }, [])
+  useClickOutside({
+    containerRef: dropdownRef,
+    childRef: menuRef,
+    isActive: menuVisible,
+    toggle: () => setMenuVisible(false)
+  })
 
   const handleSelection = (event) => {
     setMenuVisible(false)
@@ -33,9 +29,9 @@ export default function DropdownMenu(props) {
         {buttonContent}
         <CaretDown className='caret' style={{ rotate: menuVisible ? '180deg' : ''}} />
       </button>
-        <div className="dropdown-menu" onClick={handleSelection} style={{ display: menuVisible ? 'flex' : 'none'}}>
-          {children}
-        </div>
+      <div className="dropdown-menu" ref={menuRef} onClick={handleSelection} style={{ display: menuVisible ? 'flex' : 'none'}}>
+        {children}
+      </div>
     </div>
   )
 }
