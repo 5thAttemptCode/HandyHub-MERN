cd710f3bb297d36f9b7e0c4a393a5641cb7cfec5
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-09 22:06:55 +0200

    replaced outside click with custom hook

--- frontend/src/pages/handyHubberAuth/components/faq/index.jsx
+++ frontend/src/pages/handyHubberAuth/components/faq/index.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import './style.css'
 import AsideContainer from '@/components/asideContainer'
+import faqData from '@/components/data/faq'
 import SubHeader from '@/components/subHeader'
 import SubHeaderSmall from '@/components/subHeaderSmall'
-import faqData from '@/components/data/faq'
 import useClickOutside from '@/utliz/useClickOutside'
 
 
 export default function FAQ() {
 
-  const [ openedIndex, setOpenedIndex ] = useState(false)
+  const [ openedIndex, setOpenedIndex ] = useState(null)
 
+  const faqRef = useRef(null)
+
+  const active = openedIndex !== null
+
+  // toggle the list-items
   const toggleItem = (index) => {
     if (index === openedIndex) {
       setOpenedIndex(null)
@@ -19,15 +24,23 @@ export default function FAQ() {
     }
   }
 
+  // using custom hook
+  useClickOutside({
+    containerRef: faqRef,
+    childRef: faqRef,
+    isActive: active,
+    toggle: () => setOpenedIndex(null)
+  })
+
   return (
     <AsideContainer>
       <SubHeader text="Frequently asked questions" />
-      <ul className='faq-ul'>
+      <ul className='faq-ul' ref={faqRef}>
         {faqData.map((item, index) => (
           <li 
             onClick={() => toggleItem(index)} 
             key={item.id} 
-            className="faq-container"
+            className={`faq-container ${openedIndex === index ? "active" : ""}`}
           >
             <SubHeaderSmall text={item.question} />
             {openedIndex === index && (
