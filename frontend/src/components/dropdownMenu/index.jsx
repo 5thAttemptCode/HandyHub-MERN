aac295ca746e49b58764ee670ec8e6b5cb35953f
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-09 22:05:31 +0200

    replaced outside click with custom hook

--- frontend/src/components/dropdownMenu/index.jsx
+++ frontend/src/components/dropdownMenu/index.jsx
@@ -1,41 +1,50 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useRef, useState } from 'react'
 import './style.css'
+import useClickOutside from '@/utliz/useClickOutside'
 import { CaretDown } from '@phosphor-icons/react'
 
 
 export default function DropdownMenu(props) {
 
+  // Props
   const { buttonContent, children, menuVisible: propMenuVisible = false } = props
-  
+
   const dropdownRef = useRef(null)
+
   const [ menuVisible, setMenuVisible ] = useState(propMenuVisible)
 
-  const handleClickOutside = event => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setMenuVisible(false)
-    }
+  // using the custom hook
+  useClickOutside({
+    containerRef: dropdownRef,
+    childRef: dropdownRef,
+    isActive: menuVisible,
+    toggle: () => setMenuVisible(false)
+  })
+
+  // toggle the menu
+  const toggleMenuVisibility = () => {
+    setMenuVisible(prevMenuVisible => !prevMenuVisible)
   }
 
-  useEffect(() => {
-    document.addEventListener("mousedown", handleClickOutside)
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside)
-    }
-  }, [])
-
+  // close menu when clicking link
   const handleSelection = (event) => {
     setMenuVisible(false)
+    event.stopPropagation()
   }
     
   return (
     <div className="dropdown" ref={dropdownRef}>
-      <button onClick={() => setMenuVisible(prevMenuVisible => !prevMenuVisible)}>
+      <button onClick={toggleMenuVisibility}>
         {buttonContent}
         <CaretDown className='caret' style={{ rotate: menuVisible ? '180deg' : ''}} />
       </button>
-        <div className="dropdown-menu" onClick={handleSelection} style={{ display: menuVisible ? 'flex' : 'none'}}>
-          {children}
-        </div>
+      <div 
+        className="dropdown-menu"  
+        onClick={handleSelection}  
+        style={{ display: menuVisible ? 'flex' : 'none'}}
+      >
+        {children}
+      </div>
     </div>
   )
 }
