d94dbf87b52f95aae59fdcf0ee35c8a7568f4506
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-10 20:37:41 +0200

    imported new ModalButton

--- frontend/src/pages/handyHubberAuth/index.jsx
+++ frontend/src/pages/handyHubberAuth/index.jsx
@@ -2,26 +2,32 @@ import React from 'react'
 import './style.css'
 import SubHeader from '@/components/subHeader'
 import SubHeaderSmall from '@/components/subHeaderSmall'
-import ModalButton from './components/modal/components/modalButton'
 import HowItWorks from './components/howItWorks'
 import AsideContainer from '@/components/asideContainer'
 import FAQ from './components/faq'
+import ModalButton from '@/components/modal/components/modalButton'
+import HubberGrid from './components/grid'
+
 
 
 export default function HandyHubber() {
 
+  const authlId = "authModal"
+  const modalText = "Join HandyHub"
+  
   return (
     <>
       <section className='handy-auth'>
         <SubHeader text="Work Your Way" />
         <SubHeaderSmall text="You bring the skill. We'll make earning easy." />
-        <ModalButton />
+        <ModalButton id={authlId} text={modalText} />
       </section>
       <HowItWorks />
       <FAQ />
+      <HubberGrid />
       <AsideContainer backgroundColor="var(--gridContainerThree)">
         <SubHeaderSmall text="Sign up and create your first Gig today" />
-        <ModalButton />
+        <ModalButton id={authlId} text={modalText} />
       </AsideContainer>
     </>
   )
