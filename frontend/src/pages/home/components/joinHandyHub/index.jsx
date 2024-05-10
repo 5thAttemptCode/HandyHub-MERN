1207f74592a76301707f3b8e6ae982d11f68c63c
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-10 20:34:50 +0200

    imported new ModalButton

--- frontend/src/pages/home/components/joinHandyHub/index.jsx
+++ frontend/src/pages/home/components/joinHandyHub/index.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 import './style.css'
 import GridContainer from '@/components/gridContainer'
-import ModalButton from '../../../handyHubberAuth/components/modal/components/modalButton'
+import ModalButton from '@/components/modal/components/modalButton'
 
 
 export default function JoinHandyHub() {
+
+  const authlId = "authModal"
+  
   return (
     <GridContainer>
       <div className="help">
         <h1>Take your business to the <span>next level</span></h1>
-        <ModalButton />
+        <ModalButton id={authlId} text="Join HandyHub" />
         <img src="/public/image.png" alt="" />
       </div>
     </GridContainer>
