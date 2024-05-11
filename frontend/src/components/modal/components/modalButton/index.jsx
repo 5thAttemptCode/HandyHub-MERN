1d79e15592ef46aaf2064a9dd0c0d925c53bc9af
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-11 22:42:32 +0200

    adjusted props

--- frontend/src/components/modal/components/modalButton/index.jsx
+++ frontend/src/components/modal/components/modalButton/index.jsx
@@ -3,13 +3,14 @@ import './style.css'
 import { ModalContext } from '@/context/modalContext' 
 
 
-export default function ModalButton({ id, text }) {
+export default function ModalButton({ id, text, children }) {
 
   const { toggleModal } = useContext(ModalContext)
 
   return (
     <button className='modal-button green-button' onClick={() => toggleModal(id)}>
       {text}
+      {children}
     </button>
   )
 }
