e80405cbf3c40b8d69a753335caccaa3ed6bf408
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-12 16:55:52 +0200

    data update

--- frontend/src/components/data/hubberStoriesData/index.jsx
+++ frontend/src/components/data/hubberStoriesData/index.jsx
@@ -1,4 +1,4 @@
-import { HouseSimple, Lightning, PaintBrushHousehold, Wall } from "@phosphor-icons/react";
+import { HouseSimple, House, Lightning, PaintBrushHousehold, Wall } from "@phosphor-icons/react";
 
 const size = 20
 
@@ -36,7 +36,7 @@ const HubberStoriesData = [
         industry: "Mason",
         icon: <Wall size={size} />,
         stats: '"I landed 27 family house-builds within my first month."',
-        image: "https://images.pexels.com/photos/6474475/pexels-photo-6474475.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        image: "https://images.pexels.com/photos/8960996/pexels-photo-8960996.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
         story: `"Fiverr has changed my life in ways i could only dream of. I’m extremely satisfied working on this platform while 
                  supporting my family and exploring the world with new opportunities"
                `
@@ -58,26 +58,26 @@ const HubberStoriesData = [
         id: 5,
         modalID: "painter2",
         year: 2020,
-        name: "Rolanda Blanco",
-        industry: "Painter",
-        icon: <PaintBrushHousehold size={size} />,
-        stats: '"HandyHub saved my business."',
-        image: "https://images.pexels.com/photos/7218683/pexels-photo-7218683.jpeg",
-        story: `"Four years ago, I was about to lose my business. Today, I'm a top rated seller. 
-                Thanks to HandyHub my business makes more income than ever and I was able to hire more people."
+        name: "Maria Hayek",
+        industry: "Roofer",
+        icon: <HouseSimple size={size} />,
+        stats: '"Joining HandyHub was the best business decision I ever made."',
+        image: "https://images.pexels.com/photos/8961395/pexels-photo-8961395.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        story: `"HandyHub has changed my life in ways I could only dream of. I’m extremely satisfied working on this platform while 
+                 supporting my family and exploring the world with new opportunities."
                `
     },
     {
         id: 6,
         modalID: "painter3",
         year: 2020,
-        name: "Rolanda Blanco",
-        industry: "Painter",
-        icon: <PaintBrushHousehold size={size} />,
-        stats: '"HandyHub saved my business."',
-        image: "https://images.pexels.com/photos/7218683/pexels-photo-7218683.jpeg",
-        story: `"Four years ago, I was about to lose my business. Today, I'm a top rated seller. 
-                Thanks to HandyHub my business makes more income than ever and I was able to hire more people."
+        name: "Ryan Bessings",
+        industry: "Carpenter",
+        icon: <House size={size} />,
+        stats: '"The only regret I have is, to not have joined HandyHub much earlier."',
+        image: "https://images.pexels.com/photos/8817847/pexels-photo-8817847.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        story: `"I was very skeptic in the beginning because this concept was new to me. Never would I have dreamed of
+                 increasing my revenenue by 40% withing 12 month. I cant thank HandyHub enough for how much it helped me!"
                `
     }
 ]
