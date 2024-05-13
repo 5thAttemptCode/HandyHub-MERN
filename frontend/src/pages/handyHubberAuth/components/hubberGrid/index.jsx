9877dbe2f4332539cab69dcfef8f20e525efa3ca
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-13 21:09:10 +0200

    use of customHook useWindowResize

--- frontend/src/pages/handyHubberAuth/components/hubberGrid/index.jsx
+++ frontend/src/pages/handyHubberAuth/components/hubberGrid/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
 import './style.css'
 import ModalButton from '@/components/modal/components/modalButton'
-import HubberStoriesData from '../../../../components/data/hubberStoriesData'
+import HubberStoriesData from '@/components/data/hubberStoriesData'
 import SubHeaderSmall from '@/components/subHeaderSmall'
 import SubHeader from '@/components/subHeader'
 import AsideContainer from '@/components/asideContainer'
+import useWindowResize from '@/utliz/useWindowResize'
 
 
 export default function HubberGrid() {
+
+  const windowWidth = useWindowResize()
 
   return (
     <AsideContainer>
       <SubHeader text="Join our growing HandyHub community" />
       <div className="modal-button-wrapper">
-        {HubberStoriesData.map((story, index) => (
+        {HubberStoriesData.slice(0, windowWidth <= 550 ? 3 : undefined).map((story, index) => (
           <ModalButton 
             key={story.id} 
             id={story.modalID}  
