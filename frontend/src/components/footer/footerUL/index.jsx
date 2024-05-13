416c1f1e26d37b95481d0e6e577b9edb22e0698c
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-13 21:07:04 +0200

    use of customHook useWindowResize

--- frontend/src/components/footer/footerUL/index.jsx
+++ frontend/src/components/footer/footerUL/index.jsx
@@ -1,46 +1,36 @@
 import React, { useEffect, useState } from 'react'
 import './style.css'
 import { Link } from 'react-router-dom'
+import useWindowResize from '../../../utliz/useWindowResize';
 
 
 export default function FooterUL({ text, dataArray, propName, url }) {
 
-    const [ ulVisible, setUlVisible ] = useState(window.innerWidth > 600)
-    const [ isMobile, setIsMobile ] = useState(window.innerWidth <= 600)
-  
-    const handleResize = () => {
-      setIsMobile(window.innerWidth <= 600)
-      if (window.innerWidth > 600) {
-        setUlVisible(true)
-      }
-    }
-  
-    useEffect(() => {
-      window.addEventListener('resize', handleResize)
-      return () => {
-        window.removeEventListener('resize', handleResize)
-      }
-    }, [])
-  
-    const toggleVisibility = () => {
-      if(!isMobile) return;
-      setUlVisible(!ulVisible)
-    }
+  const windowResize = useWindowResize()
 
-    return (
-      <div className='footer-ul'>
-        <button onClick={toggleVisibility}>{text}</button>
-        {ulVisible && (
-          <ul>
-            {dataArray.map((item, index) => (
-              <li key={index}>
-                <Link to={item[url]}>
-                  {item[propName]}
-                </Link>
-              </li>
-            ))}
-          </ul>
-        )}
-      </div>
-    )
+  const isMobile = windowResize <= 600
+
+  const [ ulVisible, setUlVisible ] = useState(!isMobile)
+
+  const toggleVisibility = () => {
+      if (!isMobile) return
+      setUlVisible(!ulVisible)
+  }
+
+  return (
+    <div className='footer-ul'>
+      <button onClick={toggleVisibility}>{text}</button>
+      {ulVisible && (
+        <ul>
+          {dataArray.map((item, index) => (
+            <li key={index}>
+              <Link to={item[url]}>
+                {item[propName]}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  )
 }
