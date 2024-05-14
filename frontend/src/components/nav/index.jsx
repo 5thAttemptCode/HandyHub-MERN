f04e5212fd6abe1b18dda5e6c51fb33b72157568
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-14 19:16:13 +0200

    import custom hook + improved navbar

--- frontend/src/components/nav/index.jsx
+++ frontend/src/components/nav/index.jsx
@@ -1,12 +1,13 @@
-import React, { useContext } from 'react'
+import React from 'react'
 import './style.css'
 import { IndustryData } from '../data'
 import { Link } from 'react-router-dom'
 import DropdownMenu from '../dropdownMenu'
 import ExploreLink from './components/exploreLink'
 import { useAuthStore } from '@/context/AuthContext'
-import { Hamburger } from '@phosphor-icons/react'
 import ResponsiveMenu from './components/responsiveMenu'
+import SearchBar from '../searchBar'
+import useScrollVisibility from '@/utliz/useScrollVisibility'
 
 
 export default function Navbar() {
@@ -14,27 +15,39 @@ export default function Navbar() {
   //AuthContext for login/register
   const { activeTab, setActiveTab } = useAuthStore()
 
+  // toggle .sub-nav visibility
+  const showSubNav = useScrollVisibility(0.5)
+
   return (
-    <nav>
-      <Link className='logo' to="/">Handy<span>Hub</span></Link>
-      <ul>
-        <DropdownMenu
-          buttonContent="Explore "
-        >
-          {IndustryData.map(item => 
-            <ExploreLink 
-              to={`/${item.url}`} 
-              title={item.industryName} 
-              phrase={item.subText} 
-              key={item.industryName}
-            /> 
-          )}
-        </DropdownMenu>
-        <Link className='hanhub' to="/become-a-handyhubber">HandyHubber</Link>
-        <Link to="/auth" onClick={() => setActiveTab(1)}>Login</Link>
-        <Link to="/auth" onClick={() => setActiveTab(2)}>Sign Up</Link>
-        <ResponsiveMenu />
-      </ul>
-    </nav>
+    <>
+      <nav>
+        <div className="nav-container">
+          <Link className='link-logo' to="/">Handy<span>Hub</span></Link>
+          <ul>
+            <DropdownMenu
+              buttonContent="Explore "
+            >
+              {IndustryData.map(item => 
+                <ExploreLink 
+                  to={`/${item.url}`} 
+                  title={item.industryName} 
+                  phrase={item.subText} 
+                  key={item.industryName}
+                /> 
+              )}
+            </DropdownMenu>
+            <Link className='hanhub' to="/become-a-handyhubber">HandyHubber</Link>
+            <Link to="/auth" onClick={() => setActiveTab(1)}>Login</Link>
+            <Link to="/auth" onClick={() => setActiveTab(2)}>Sign Up</Link>
+            <ResponsiveMenu />
+          </ul>
+        </div>
+        {showSubNav &&
+          <div className="sub-nav">
+            <SearchBar />
+          </div>
+        }
+      </nav>
+    </>
   )
 }
