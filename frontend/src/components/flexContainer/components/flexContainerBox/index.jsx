9dea32fd4692b0d49cfd25bbe154fe155125ef04
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-19 14:47:40 +0200

    added className prop

--- frontend/src/components/flexContainer/components/flexContainerBox/index.jsx
+++ frontend/src/components/flexContainer/components/flexContainerBox/index.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import './style.css'
 
 
-export default function FlexContainerBox({ children }) {
+export default function FlexContainerBox({ children, className }) {
   return (
-    <div className='flex-container-box'>
+    <div className={`flex-container-box ${className}`}>
       {children}
     </div>
   )
