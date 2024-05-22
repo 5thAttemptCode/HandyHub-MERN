fef82d77581ab68dbc387ffe6fbeeffba8fa1231
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-22 13:03:12 +0200

    fixed Modal-bug and added comments

--- frontend/src/pages/handyHubberAuth/components/register/index.jsx
+++ frontend/src/pages/handyHubberAuth/components/register/index.jsx
@@ -5,37 +5,63 @@ import { useCookies } from 'react-cookie'
 import { useNavigate } from 'react-router-dom'
 import { useAuthStore } from '@/context/AuthContext'
 import { ToastError, ToastSuccess } from "@/components/toastMessage"
+import { ModalContext } from '@/context/modalContext'
 
 
 export default function HandyRegister() {
 
+  // useCookies hook to manage cookies, specifically access_token
+  // **
   const [ _, setCookies ] = useCookies(["access_token"])
 
+  // Manage user email and password state
   const [ userMail, setHubberMail ] = useState("")
   const [ password, setPassword ] = useState("")
   
+  // Navigate to other routes
   const navigate = useNavigate() 
 
+  // Accessing setHandyHubberUser from the authentication context
   const { setHandyHubberUser } = useAuthStore()
+
+  // Accessing toggleModal from the modal context to control modal visibility
+  const { toggleModal } = useContext(ModalContext)
   
+  // Handle form submission
   const onSubmit = async (event, receivedUserMail, receivedPassword) => {
     event.preventDefault()
 
     try{
+      // Making an API call to register the user
       const result = await axios.post("http://localhost:3001/become-a-handyhubber/register", {
         userMail: receivedUserMail,
         password: receivedPassword
       })
-      
+
+      // On successful register, set the access_token cookie
       setCookies('access_token', result.data.token)
+
+      // Store a flag in local storage to indicate the user is logged in
       window.localStorage.setItem('handyHubberUser', true)
+
+      // Update the auth context to reflect the user's logged-in status
       setHandyHubberUser(true)
+
+      // Navigate to the user's profile page
       navigate("/register-form")
+
+      // Show a success toast message
       ToastSuccess({ message: "Business registrated successful!" })
+
     } catch(err){
+      // Handle errors by logging and showing an error toast message
       console.error(err)
       console.error(err.response)
       ToastError({ message: "Whoopsy, try again in a few!" })
+      
+    } finally {
+      // Ensure modal is toggled off after the process
+      toggleModal("authModal")
     }
   }
 
