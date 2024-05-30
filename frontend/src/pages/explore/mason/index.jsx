a727eddd9ec4f01034043e1139f81fbb1ef70ce8
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-30 19:02:12 +0200

    import CompanyList component - cleaner code

--- frontend/src/pages/explore/mason/index.jsx
+++ frontend/src/pages/explore/mason/index.jsx
@@ -1,49 +1,30 @@
 import React from 'react'
 import '../style.css'
-import CompanyContainer from '@/components/companyContainer'
 import PageHeader from '@/components/pageHeader'
 import { IndustryData } from '@/components/data'
 import { useLocation } from 'react-router-dom'
 import useFetchCompanyData from '@/utliz/usePostCompany'
+import CompanyList from '../exploreComponents/companyList'
 
 
 export default function Mason() {
 
   const { industryName, mainText } = IndustryData.find(item => item.industryName === "Mason")
-
-  // Use the useLocation hook to access the current location object, which includes the state passed via navigation
+  
   const location = useLocation()
-  
-  // Destructure the state from the location object. This state contains the newly registered company data if available
   const { state } = location
-  
-  // Use the custom hook useFetchCompanyData to fetch the list of companies for the "roofer" industry
   const { companies, loading, error } = useFetchCompanyData('mason')
 
-  // If the data is still loading, display a loading message
-  if (loading) return <p>Loading...</p>
-  
-  // If there was an error during the fetch, display an error message
-  if (error) return <p>Error: {error.message}</p>
-
-  // Combine the fetched companies with the newly registered company from the state if it exists
   const allCompanies = state ? [...companies, state] : companies
 
- return (
-   <section>
-     <PageHeader textH1={industryName} textH4={mainText} />
-     <ul>
-       {allCompanies.map((company, index) => (
-         <li key={`${company.companyMail}-${index}`}>
-          <CompanyContainer to="/">
-            <p>Name: {company.companyName}</p>
-            <p>Phone: {company.companyPhone}</p>
-            <p>Email: {company.companyMail}</p>
-            <p>Description: {company.companyDescription}</p>
-          </CompanyContainer>
-         </li>
-       ))}
-     </ul>
-   </section>
- )
+  return (
+    <section>
+      <PageHeader textH1={industryName} textH4={mainText} />
+      <CompanyList
+        companies={allCompanies} 
+        loading={loading} 
+        error={error} 
+      />
+    </section>
+  )
 }
