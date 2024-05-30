c6746e6740984e940dabb22f3a9c95288f6c7c35
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-05-30 19:03:24 +0200

    fetch func in sep comp. + cond. rendering for user

---
+++ frontend/src/pages/explore/exploreComponents/companyList/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import '../../style.css'
+import CompanyContainer from '@/components/companyContainer'
+import { useAuthStore } from "@/context/AuthContext"
+
+
+export default function CompanyList({ companies, loading, error }) {
+
+  const { user } = useAuthStore()
+
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error: {error.message}</p>
+
+  return (
+    <ul className='company-ul'>
+      {companies.map((company, index) => (
+        <li key={`${company.companyMail}-${index}`}>
+          <CompanyContainer to="/">
+            <p>Name: {company.companyName}</p>
+            {user && (
+              <>
+                <p>Phone: {company.companyPhone}</p>
+                <p>Email: {company.companyMail}</p>
+              </>
+            )}
+            <p>Description: {company.companyDescription}</p>
+          </CompanyContainer>
+        </li>
+      ))}
+    </ul>
+  )
+}
