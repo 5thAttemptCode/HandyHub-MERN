2a81261e786d6622b847f8eadaf42f81d015b9bd
Henry <91667944+5thAttemptCode@users.noreply.github.com>
2024-06-02 21:18:35 +0200

    added styling

--- frontend/src/pages/explore/exploreComponents/exploreNav/index.jsx
+++ frontend/src/pages/explore/exploreComponents/exploreNav/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
+import './style.css'
 import DropdownMenu from '@/components/dropdownMenu'
 
 
 export default function ExploreNav({ selectedOption, onChange }) {
 
   return (
-    <DropdownMenu buttonContent={selectedOption}>
-      <option onClick={() => onChange("A - Z")}>A - Z</option>
-      <option onClick={() => onChange("Price low to high")}>Price low to high</option>
-      <option onClick={() => onChange("Price high to low")}>Price high to low</option>
-    </DropdownMenu>
+    <div className="explore-nav">
+      <DropdownMenu buttonContent={selectedOption}>
+        <option onClick={() => onChange("A - Z  ")}>A - Z</option>
+        <option onClick={() => onChange("Price low to high  ")}>Price low to high</option>
+        <option onClick={() => onChange("Price high to low  ")}>Price high to low</option>
+      </DropdownMenu>
+    </div>
   )
 }
